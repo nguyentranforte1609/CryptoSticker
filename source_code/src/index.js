d1420cbbbaf957a0b8fa546b9d693c0e20646460
Nguyen Tran <nguyen.tran.forte1609@mail.com>
2018-05-15 22:00:57 +0700

    Recreate project directory with create-react-app

--- source_code/src/index.js
+++ source_code/src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import "./App.css";
-import Tickers from "../components/Tickers.js";
+import Tickers from "./components/Tickers.js";
 
 class App extends React.Component {
   render() {

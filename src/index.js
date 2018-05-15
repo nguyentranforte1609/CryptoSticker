44fec45f2af65a41e170bc547eadf59cdff702fb
Nguyen Tran <nguyen.tran.forte1609@mail.com>
2018-05-15 21:41:48 +0700

    Add source code | Simple UI

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { render } from "react-dom";
+import "./App.css";
+import Tickers from "../components/Tickers.js";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Cryptocurrency Ticker</h2>
+        </div>
+        <Tickers />
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
